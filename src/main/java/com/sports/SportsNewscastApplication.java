877ff555adf748e27ab78e2ed2ec7be924273bfe
Breno Castelo Branco <brenocastelo10@hotmail.com>
2018-07-03 23:30:01 -0300

    criação do projeto

---
+++ src/main/java/com/sports/SportsNewscastApplication.java
@@ -0,0 +1,12 @@
+package com.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsNewscastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsNewscastApplication.class, args);
+	}
+}
