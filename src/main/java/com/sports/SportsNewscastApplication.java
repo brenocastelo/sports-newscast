62eabd56c07784c53287b9a475669d6173a11d05
Breno Castelo Branco <brenocastelo10@hotmail.com>
2018-07-10 14:24:37 -0300

    alteração no layout

--- src/main/java/com/sports/SportsNewscastApplication.java
+++ src/main/java/com/sports/SportsNewscastApplication.java
@@ -2,11 +2,19 @@ package com.sports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @SpringBootApplication
 public class SportsNewscastApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportsNewscastApplication.class, args);
+	}
+	
+	@Bean
+	LayoutDialect layoutDialect() {
+		return new LayoutDialect();
 	}
 }
