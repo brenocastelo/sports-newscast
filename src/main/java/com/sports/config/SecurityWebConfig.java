630a2a5cde83ef790d2ed818a54527b697718999
Breno Castelo Branco <brenocastelo10@hotmail.com>
2018-07-09 23:46:54 -0300

    início da alteração do template

--- src/main/java/com/sports/config/SecurityWebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.sports.config;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-public class SecurityWebConfig extends WebSecurityConfigurerAdapter{
-
-}
